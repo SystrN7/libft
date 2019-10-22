414deb75d73391321b405d3aa766d3318c6caa6b
Felix Galaup <fgalaup@z2r1p1.le-101.fr>
2019-10-22 11:30:34 +0200

    Fix little error on memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/08 14:02:51 by fgalaup      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/21 10:40:30 by fgalaup     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/22 08:37:35 by fgalaup     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -15,15 +15,15 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t				i;
-	unsigned char		*dest_bytes;
-	unsigned char const	*src_bytes;
+	size_t			i;
+	unsigned char	*dest_bytes;
+	unsigned char	*src_bytes;
 
-	if (dst == NULL || src == NULL)
+	if (dst == NULL && src == NULL)
 		return (NULL);
 	i = 0;
-	dest_bytes = dst;
-	src_bytes = src;
+	dest_bytes = (unsigned char *)dst;
+	src_bytes = (unsigned char *)src;
 	while (i < n)
 	{
 		dest_bytes[i] = src_bytes[i];
