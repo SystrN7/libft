f659d2350453ff24c458bb369f091206445cd8a1
Felix Galaup <fgalaup@z2r1p1.le-101.fr>
2019-10-22 10:49:36 +0200

    Replace memcpy per strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/09 16:39:07 by fgalaup      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/15 09:57:27 by fgalaup     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/22 10:28:38 by fgalaup     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,11 +18,9 @@ char	*ft_strdup(const char *s1)
 	void		*str_clone;
 	size_t		str_lenght;
 
-	if (s1 == NULL)
-		str_lenght = 0;
 	str_lenght = ft_strlen(s1);
 	if (!(str_clone = malloc((str_lenght + 1) * sizeof(char))))
 		return (NULL);
-	ft_strlcpy(str_clone, s1, str_lenght + 1);
+	ft_memcpy(str_clone, s1, str_lenght + 1);
 	return (str_clone);
 }
