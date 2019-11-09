b192e285e26ccad35722a810f85186ad24028c0d
Felix Galaup <fgalaup@z1r13p6.le-101.fr>
2019-11-09 15:45:58 +0100

    add security (del) to the del_link in list

--- List/ft_lstdel_link.c
+++ List/ft_lstdel_link.c
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/11 10:59:10 by fgalaup      #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/07 15:41:07 by fgalaup     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/09 08:39:09 by fgalaup     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,7 +17,8 @@ void	ft_lstdel_link(t_list *lst, void (*del)(void*))
 {
 	if (lst == NULL || lst->content == NULL)
 		return ;
-	del(lst->content);
+	if (del != NULL)
+		del(lst->content);
 	free(lst);
 	lst = NULL;
 }
