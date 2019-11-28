80a38fa24ae9bb5f0444cae12a98d73ed33b8577
Felix Galaup <felixgalaup@yahoo.fr>
2019-11-28 15:29:52 +0100

    Fix bug in get file extention if file name dont have dot.

--- File/ft_file_get_extention.c
+++ File/ft_file_get_extention.c
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/27 13:30:31 by fgalaup      #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/27 16:15:12 by fgalaup     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/28 15:27:17 by fgalaup     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,7 +17,8 @@ char	*ft_file_get_extention(char *file_name)
 {
 	char	*last_dot;
 
-	last_dot = ft_strrchr(file_name, '.');
+	if (!(last_dot = ft_strrchr(file_name, '.')))
+		return ("");
 	if (!ft_strchr(last_dot, '/'))
 		return (last_dot);
 	return ("");
