30d4aea0c28e3cac79eae66f8339c8f73750a7ac
Felix Galaup <felixgalaup@yahoo.fr>
2020-01-22 10:42:01 +0100

    Add comment to ft_nothing.

--- Memory/ft_nothing.c
+++ Memory/ft_nothing.c
@@ -6,10 +6,19 @@
 /*   By: fgalaup <fgalaup@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/01/02 09:23:48 by fgalaup      #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/02 09:25:27 by fgalaup     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/05 11:43:14 by fgalaup     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** Function : ft_nothing
+** Decription : function do nothing
+** Parameter :
+**  - address : just a generic address
+*/
 
 void	ft_nothing(void *address)
 {
