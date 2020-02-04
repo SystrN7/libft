9b5d57795947d8de648f7bbf25b08fb308d51c92
Felix Galaup <felixgalaup@yahoo.fr>
2020-02-04 14:38:32 +0100

    An other fix

--- Type/Convert/ft_strtoll.c
+++ Type/Convert/ft_strtoll.c
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/15 08:09:23 by fgalaup      #+#   ##    ##    #+#       */
-/*   Updated: 2020/02/04 14:34:27 by fgalaup     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/04 14:38:20 by fgalaup     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -26,6 +26,7 @@ size_t		ft_strtoll(char *str, long long *number)
 	*number = 0;
 	while (str[i] >= '0' && str[i] <= '9')
 		*number = (*number * 10) + (str[i++] - '0');
-	*number = *number * (-1 + !negative);
+	if (negative)
+		*number = -(*number);
 	return (i);
 }
