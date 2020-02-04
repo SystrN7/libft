361100af6a0284f17ae20906c61bfde201e47862
Felix Galaup <felixgalaup@yahoo.fr>
2020-02-04 14:24:49 +0100

    Fix strtoll to support negative number.

--- Type/Convert/ft_strtoll.c
+++ Type/Convert/ft_strtoll.c
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/15 08:09:23 by fgalaup      #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/15 08:10:58 by fgalaup     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/04 14:23:12 by fgalaup     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,10 +16,16 @@
 size_t		ft_strtoll(char *str, long long *number)
 {
 	size_t i;
+	t_bool negative;
 
 	i = 0;
+	negative = 0;
+	while (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			negative = !negative;
 	*number = 0;
 	while (str[i] >= '0' && str[i] <= '9')
 		*number = (*number * 10) + (str[i++] - '0');
+	*number = *number * (-1 * negative);
 	return (i);
 }
