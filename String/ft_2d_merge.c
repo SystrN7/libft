556ef40bfac6c89f60c8f60edee59c392e5235d8
Felix Galaup <felixgalaup@yahoo.fr>
2021-03-01 16:53:42 +0100

    [^]-[String](2D)-Merge backslash.

--- String/ft_2d_merge.c
+++ String/ft_2d_merge.c
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/03 10:31:22 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/02/03 10:59:13 by fgalaup          ###   ########lyon.fr   */
+/*   Updated: 2021/02/17 12:41:48 by fgalaup          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,12 @@ char	*ft_2d_merge(char **array)
 	size_t	string_lenght;
 	size_t	i;
 
-	string_lenght = 0;
+	string_lenght = 1;
 	i = 0;
 	while (array[i])
-		string_lenght = ft_strlen(array[i++]);
+		string_lenght += ft_strlen(array[i++]);
 	string = ft_managed_malloc(string_lenght);
+	ft_memset(string, 0, string_lenght);
 	if (string == NULL)
 		return (NULL);
 	i = 0;
