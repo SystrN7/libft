2602da27e4cb3e2b8245cdd0cd87ba214206b1fd
Felix Galaup <felixgalaup@yahoo.fr>
2021-05-18 14:57:32 +0200

    [^]-[Type](Strtoll)-count right number of digit.

--- Type/Convert/ft_strtoll.c
+++ Type/Convert/ft_strtoll.c
@@ -6,7 +6,7 @@
 /*   By: fgalaup <fgalaup@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/15 08:09:23 by fgalaup           #+#    #+#             */
-/*   Updated: 2021/05/07 17:15:08 by fgalaup          ###   ########lyon.fr   */
+/*   Updated: 2021/05/18 14:56:41 by fgalaup          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 size_t	ft_strtoll(const char *str, long long *number)
 {
 	size_t	i;
+	size_t	j;
 	t_bool	negative;
 
 	i = 0;
+	j = 0;
 	negative = 0;
-	while (str[i] == '+' || str[i] == '-')
-		if (str[i++] == '-')
+	while (str[j] == '+' || str[j] == '-')
+		if (str[j++] == '-')
 			negative = !negative;
 	*number = 0;
-	while (str[i] >= '0' && str[i] <= '9')
-		*number = (*number * 10) + (str[i++] - '0');
+	while (str[j + i] >= '0' && str[j + i] <= '9')
+		*number = (*number * 10) + (str[j + i++] - '0');
 	if (negative)
 		*number = -(*number);
 	return (i);
